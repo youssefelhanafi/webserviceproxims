a327c4dcccb192e295bc5971098ff947a5ec88fa
youssefelhanafi <38573527+youssefelhanafi@users.noreply.github.com>
2018-06-05 22:29:40 +0000

    Add files via upload

---
+++ android(1).sql
@@ -0,0 +1,178 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  ven. 01 juin 2018 à 15:51
+-- Version du serveur :  10.1.28-MariaDB
+-- Version de PHP :  7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `android`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `localisation`
+--
+
+CREATE TABLE `localisation` (
+  `id` int(11) NOT NULL,
+  `longtitude` varchar(60) NOT NULL,
+  `latitude` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `localisation`
+--
+
+INSERT INTO `localisation` (`id`, `longtitude`, `latitude`) VALUES
+(1, '11', '11');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(20) DEFAULT NULL,
+  `lastname` varchar(20) DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `birthday` date NOT NULL,
+  `phone` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `email`, `address`, `birthday`, `phone`) VALUES
+(1, 'Youssef', 'Elhanafi', 'mrtshakur', '1a1dc91c907325c69271ddf0c944bc72', 'youssef.elhanafi@gmail.com', 'Asfi', '1996-06-07', ''),
+(2, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', '', '', '0000-00-00', ''),
+(3, NULL, NULL, '', '', '', NULL, '0000-00-00', ''),
+(6, 'meriyem', 'boujellal', 'mery', '1a1dc91c907325c69271ddf0c944bc72', 'meryem@email.com', 'asfi', '1990-07-01', '0640394423'),
+(7, 'ahmed', 'salhi', 'mido', '1a1dc91c907325c69271ddf0c944bc72', 'ahmed@gmail.com', 'asfi', '1980-06-12', '0657432156'),
+(8, 'test', 'test', 'test', '098f6bcd4621d373cade4e832627b4f6', 'test@ted.com', 'test', '1996-01-01', '0670876543');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users0`
+--
+
+CREATE TABLE `users0` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users0`
+--
+
+INSERT INTO `users0` (`id`, `username`, `password`, `email`) VALUES
+(4, 'mrtshakur', '1a1dc91c907325c69271ddf0c944bc72', 'marechal1marocain'),
+(5, 'youssef', '1a1dc91c907325c69271ddf0c944bc72', 'youssef.elhanafi@gmail.com'),
+(6, 'test', '098f6bcd4621d373cade4e832627b4f6', 'test'),
+(7, 'test0', 'f6f4061a1bddc1c04d8109b39f581270', 'test0'),
+(8, 'mrtshakur02', '1a1dc91c907325c69271ddf0c944bc72', 'youssef.elhanafi');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id_utilisateur` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prénom` varchar(255) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `num_tel` varchar(255) NOT NULL,
+  `sexe` enum('M','F') DEFAULT NULL,
+  `ville` varchar(255) DEFAULT NULL,
+  `pays` varchar(255) DEFAULT NULL,
+  `date_naissance` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `localisation`
+--
+ALTER TABLE `localisation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Index pour la table `users0`
+--
+ALTER TABLE `users0`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id_utilisateur`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `localisation`
+--
+ALTER TABLE `localisation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT pour la table `users0`
+--
+ALTER TABLE `users0`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
